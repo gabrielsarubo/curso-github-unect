f71a01f8ecfb2c805b9cb40a0c24e8a025982050
Gabriel Sousa <gabrielsarubo@hotmail.com>
2019-09-22 11:35:12 -0300

    Add fuction sum up

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int somar(int a, int b) {
+    return a + b;
+}
+
 int main(int argc, char const *argv[]) {
     int a = atoi(argv[1]);
     int b = atoi(argv[2]);
